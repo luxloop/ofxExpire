c5f96cb80b1bc0d15893fe829176b351bd194fdc
Ivaylo Getov <contact@ivaylogetov.com>
2015-08-12 01:46:33 -0700

    cleanup example

--- example_ofxExpire/src/ofApp.cpp
+++ example_ofxExpire/src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup(){
     /*
       Check in setup() if app is "expired"
-      Required argumenrs are the month (the names are predefined), day, and year
+      Required arguments are the month (the names are predefined), day, and year
       of the last day the app will work.
      
       The default alert message is "This app has expired and will now quit."
@@ -60,50 +60,5 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
